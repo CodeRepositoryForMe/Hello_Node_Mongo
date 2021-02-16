3c239caa3c72ab08329972ea8a55470eae827e87
SujayM <SujayMane@Hotmail.com>
2021-02-16 07:14:22 +0530

    Added code for Update and Delete Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,55 +1,99 @@
 const { v4: uuidv4 } = require("uuid");
 const Product = require("../models/product");
+const mongoDB = require("mongodb");
+
+const ObjectId = mongoDB.ObjectId;
 
 // Add product to Catelog
 exports.exePostProducts = (req, res, next) => {
   console.log("Add product to Catelog !!");
-  
+
   console.log(req.body);
 
   const title = req.body.Title;
   const price = req.body.Cost;
   const description = req.body.Description;
   const imageURL = req.body.ProductLink;
-console.log(title +"---"+ price + "---" + description +" --- "+ imageURL);
   const product = new Product(title, price, description, imageURL);
-  product.save().then().catch();
-  res.redirect("/catelog");
+  product
+    .save()
+    .then((result) => {
+      res.redirect("/catelog");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Get list of products to select product for update
 exports.exeGetToPutProduct = (req, res, next) => {
   console.log("Update existing product !!");
-
-  res.render("admin/products", {
-    pageTitle: "Edit Product",
-    //prods: products,
-    pageName: "editProduct",
-    catelog: true,
-  });
+  Product.fetchAll()
+    .then((products) => {
+      res.render("admin/products", {
+        pageTitle: "Edit Product",
+        prods: products,
+        pageName: "editProduct",
+        catelog: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Get selected product details
 exports.exeGetToUpdateProduct = (req, res, next) => {
-  console.log("Show selected product details!!");
-
-  res.render("admin/edit-product", {
-    pageTitle: "Update Product",
-    //prod: result,
-    pageName: "updateProduct",
-  });
+  const productID = req.params.productid;
+  Product.findByID(productID)
+    .then((product) => {
+      res.render("admin/edit-product", {
+        pageTitle: "Update Product",
+        prod: product,
+        pageName: "updateProduct",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Update selected product in product catelog
 exports.exePutProduct = (req, res, next) => {
-  console.log("Update this product");
+  const title = req.body.Title;
+  const price = req.body.Cost;
+  const description = req.body.Description;
+  const imageURL = req.body.ProductLink;
+  const id = req.body.productid;
 
-  res.redirect("/editProduct");
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageURL,
+    new ObjectId(id)
+  );
+  product
+    .save()
+    .then((result) => {
+      res.redirect("/editProduct");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Delete selected product from product catelog
 exports.exeDeleteProduct = (req, res, next) => {
   console.log("Delete this product");
-
+  const productID = req.body.productid;
+  console.log(productID);
+  Product.deleteByID(productID)
+    .then((result) => {
+        res.redirect("/editProduct");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   res.redirect("/editProduct");
 };
--- models/product.js
+++ models/product.js
@@ -2,19 +2,25 @@ const mongoDB = require("mongodb");
 const getDB = require("../util/database").getDB;
 
 class Product {
-  constructor(title, price, description, imageURL) {
+  constructor(title, price, description, imageURL, id) {
     this.title = title;
     this.cost = price;
     this.description = description;
     this.url = imageURL;
+    this._id = id;
   }
 
   save() {
     const db = getDB();
-    console.log(this);
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -40,7 +46,6 @@ class Product {
 
   static findByID(productID) {
     const db = getDB();
-    console.log("In find by ID");
     console.log(productID);
     return db
       .collection("products")
@@ -49,6 +54,20 @@ class Product {
       .then((products) => {
         console.log(products);
         return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteByID(productID) {
+    const db = getDB();
+    console.log(productID);
+    return db.collection("products")
+      .deleteOne({ _id: new mongoDB.ObjectId(productID) })
+      .then((result) => {
+        console.log("deleted");
+        return result;
       })
       .catch((err) => {
         console.log(err);
